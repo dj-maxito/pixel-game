cd2f7321c4c5691da264e2f7e5372f767ab65d3d
dj-maxito <maxameerjefferson.iy@gmail.com>
2025-12-23 03:14:07 -0300

    Inicialización de proyecto

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex pt-30 flex-col">
+      <h1 className="text-3xl "> Hola 2</h1>
+      <button>Jugar</button>
+      <button>Salir</button>
+    </div>
+  );
+}
